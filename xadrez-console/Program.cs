46e0f5e6e07fd13b398c1b7036d2da5f0a9d6809
Felipe Cavalcante <zfelp.cs@gmail.com>
2025-05-09 14:47:40 -0300

    Meu projeto de Xadrez em C#

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console {
+    internal class Program {
+        private static void Main(string[] args) {
+
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada) {
+
+                    try {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        _ = Console.ReadLine(); // Fix: Use the return value of Console.ReadLine() or remove the line if not needed.  
+                    }
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            _ = Console.ReadLine();
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,55 @@
+﻿namespace tabuleiro {
+    internal class Tabuleiro {
+
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas) {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca Peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
+
+        public Peca Peca(Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool ExistePeca(Posicao pos) {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos) {
+            if (ExistePeca(pos)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
+
+        public Peca RetirarPeca(Posicao pos) {
+            if (Peca(pos) == null) {
+                return null;
+            }
+            Peca aux = Peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        }
+
+        public bool PosicaoValida(Posicao pos) {
+            return pos.linha >= 0 && pos.linha < Linhas && pos.coluna >= 0 && pos.coluna < Colunas;
+        }
+
+        public void ValidarPosicao(Posicao pos) {
+            if (!PosicaoValida(pos)) {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
+    }
+}
